492a6b075c431f7faa7f272e73c99946b5192454
Yilou Wang <wangyilou123@gmail.com>
2024-12-09 11:38:33 +0100

    Add and adjust tests

--- planv_tests/feature_tests/constrained_random/random_variables/Array/t_constraint_assoc_arr_basic.sv
+++ planv_tests/feature_tests/constrained_random/random_variables/Array/t_constraint_assoc_arr_basic.sv
@@ -1,10 +1,12 @@
 module t_constraint_assoc_arr_basic;
     class assoc;
-        rand bit [7:0] aa[int] = '{0:22, 16:32, 12:3};
-        constraint c { foreach(aa[ii]) aa[ii] < 10; }
-
+        rand bit [7:0] aa[string] = '{"XiaoMing":22, "MeiLi":32, "PanHong":31};
+        constraint c {  // foreach (aa[ii]) aa[ii] < 10;  Foreach string situation, select always shows #x, unsupported issue
+                    aa["XiaoMing"] inside {11, 12, 13};
+                    aa["MeiLi"] == 66; 
+                        }
         function display();
-            foreach (aa[ii]) $display("Value is %d", aa[ii]);
+            foreach (aa[ii]) $display("The Value of aa[%s] is %d", ii, aa[ii]);
         endfunction
     endclass
 
